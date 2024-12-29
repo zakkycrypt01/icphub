dd6435189d05d69b222d968b95e3f4ffc4a71166
zakkycrypt01 <zakkycrypt@gmail.com>
2024-12-29 18:06:17 +0100

    Update homepage heading size for improved visibility

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
-                <h1 className="text-2xl pb-3 font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none bg-gradient-to-r from-[#6A85F1] to-[#C572EF] bg-clip-text text-transparent">
+                <h1 className="text-3xl pb-3 font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none bg-gradient-to-r from-[#6A85F1] to-[#C572EF] bg-clip-text text-transparent">
                   Where community meets innovation ⚡
                 </h1>
                 <p className="mx-auto max-w-[700px] text-muted-foreground md:text-xl">
